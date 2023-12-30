1ea560bca389def3c44edde25ab8e0ade7cc7786
dinuhifi <dinesh23110077@snuchennai.edu.in>
2023-12-30 16:05:04 +0530

    Array ADT using C

--- Array ADT/array-adt.c
+++ Array ADT/array-adt.c
@@ -47,7 +47,7 @@ void insert(struct Array * arr, int index, int x){
 
 void delete(struct Array * arr, int index){
     if(check_underflow(arr) == 1){
-        if(index < arr->length && index >=0){
+        if(index <= arr->length && index >=0){
             for(int i=index; i<arr->length-1; i++) arr->A[i] = arr->A[i+1];
             arr->length--;
         }
@@ -55,6 +55,85 @@ void delete(struct Array * arr, int index){
     }
     else printf("Array is empty\n");
     display(arr);
+}
+
+void linear_search(struct Array * arr, int key){
+    if(check_underflow(arr) == 1){
+        for(int i=0; i<arr->length; i++){
+            if(arr->A[i] == key){
+                printf("Element found at index %d\n", i);
+                return;
+            }
+    }
+    printf("Element not found\n");
+    }
+    else printf("Array is empty\n");
+}
+
+void binary_search(struct Array * arr, int key){
+    if(check_underflow(arr)==1){
+        int l=0, h=arr->length-1, mid;
+        while(l<=h){
+            mid = (l+h)/2;
+            if(arr->A[mid] == key){
+                printf("element found at index %d\n", mid);
+                return;
+            }
+            else if(arr->A[mid] < key) l = mid+1;
+            else h = mid-1;
+        }
+        printf("Element not found\n");
+    }
+    else printf("Array is empty\n");
+}
+
+void get(struct Array * arr, int index){
+    if(check_underflow(arr) == 1){
+        if(index < arr->length && index >=0) printf("%d\n", arr->A[index]);
+        else printf("Invalid index\n");
+    }
+    else printf("Array is empty\n");
+}
+
+void set(struct Array * arr, int index, int x){
+    if(check_underflow(arr) == 1){
+        if(index < arr->length && index >=0) arr->A[index] = x;
+        else printf("Invalid index\n");
+    }
+    else printf("Array is empty\n");
+    display(arr);
+}
+
+void max_min(struct Array * arr){
+    if(check_underflow(arr)==1){
+        int max,min = arr->A[0];
+        for(int i=1; i<arr->length; i++){
+            if(arr->A[i] > max) max = arr->A[i];
+            else if(arr->A[i] < min) min = arr->A[i];
+        }
+        printf("Max element is %d\n", max);
+        printf("Min element is %d\n", min);
+    }
+    else printf("Array is empty\n");
+}
+
+void sum(struct Array * arr){
+    if(check_underflow(arr)==1){
+        int total = 0;
+        for(int i=0; i<arr->length; i++) total+=arr->A[i];
+        printf("Sum of all elements is %d\n", total);
+    }
+    else printf("Array is empty\n");
+}
+
+void avg(struct Array * arr){
+    if(check_underflow(arr)==1){
+        float total = 0;
+        for(int i=0; i<arr->length; i++) total+=arr->A[i];
+        total = total/arr->length;
+        printf("Avg of all elements is %.2f\n", total);
+    }
+    else printf("Array is empty\n");
 }
 
 int main() {
@@ -73,10 +152,6 @@ int main() {
     arr.length = n;
     
     display(&arr);
-    append(&arr, 10);
-    insert(&arr, 3, 20);
-    delete(&arr, 1);
-    printf("Length of array: %d\n", arr.length);
 
     return 0;
 }
